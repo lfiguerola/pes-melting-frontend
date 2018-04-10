aadc0bff0b4d1896b179444b62c4b92989aa136f
Pau Carbonell <uapllenobrac@gmail.com>
2018-04-10 17:45:49 +0200

    Create the register page and modify the View model to use a button

--- MeltingApp/MeltingApp/ViewModels/LoginViewModel.cs
+++ MeltingApp/MeltingApp/ViewModels/LoginViewModel.cs
@@ -12,12 +12,19 @@ namespace MeltingApp.ViewModels
         {
             navigationService = DependencyService.Get<INavigationService>(DependencyFetchTarget.GlobalInstance);
             LoginCommand = new Command(HandleLoginCommand);
+            RegisterCommand = new Command(HandleRegisterCommand);
         }
         public Command LoginCommand { get; set; }
+        public Command RegisterCommand { get; set; }
 
         void HandleLoginCommand()
         {
             navigationService.SetRootPage<MainPage>();
+        }
+
+        void HandleRegisterCommand()
+        {
+            //Crida a resgister
         }
     }
 }

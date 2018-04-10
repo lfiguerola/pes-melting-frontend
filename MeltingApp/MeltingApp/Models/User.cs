d3f3250985f7393349ccf7db4e97d5a8543f281f
lfiguerola <laurafipe@gmail.com>
2018-04-10 15:58:27 +0200

    onpropertychanged added in User model

--- MeltingApp/MeltingApp/Models/User.cs
+++ MeltingApp/MeltingApp/Models/User.cs
@@ -9,27 +9,67 @@ namespace MeltingApp.Models
         /// <summary>
         /// the user name
         /// </summary>
-        public string username { get; set; }
+        public string username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                OnPropertyChanged(nameof(username));
+            }
+        }
 
         /// <summary>
         /// the user email in format user@example.com
         /// </summary>
-        public string email { get; set; }
+        public string email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged(nameof(email));
+            }
+        }
 
         /// <summary>
         /// the user password
         /// </summary>
-        public string password { get; set; }
+        public string password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged(nameof(password));
+            }
+        }
 
         /// <summary>
         /// indicates if this user is activated or not
         /// </summary>
-        public bool activated { get; set; }
+        public bool activated
+        {
+            get { return activated; }
+            set
+            {
+                activated = value;
+                OnPropertyChanged(nameof(activated));
+            }
+        }
 
         /// <summary>
         /// 
         /// </summary>
-        public string code { get; set; }
+        public string code
+        {
+            get { return code; }
+            set
+            {
+                code = value;
+                OnPropertyChanged(nameof(code));
+            }
+        }
 
     }
 }

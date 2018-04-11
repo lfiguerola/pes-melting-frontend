ff3e8df6e3b6c06600c21d60da06bc7c5eb7d369
lfiguerola <laurafipe@gmail.com>
2018-04-12 01:47:38 +0200

    fixed some missing files

---
+++ MeltingApp/MeltingApp/Interfaces/IOperatingSystemMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeltingApp.Interfaces
+{
+    /// <summary>
+    /// Interface that exposes the Operating System methods
+    /// </summary>
+    public interface IOperatingSystemMethods
+    {
+        /// <summary>
+        /// Shows toasts with an specific text
+        /// </summary>
+        /// <param name="text"></param>
+        void ShowToast(string text);
+    }
+}

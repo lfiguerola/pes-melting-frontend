52115cd4575b652248b33b27d27ba5baad1e4ced
Pau Carbonell <uapllenobrac@gmail.com>
2018-04-11 21:53:58 +0200

    Add the code confirmation part

---
+++ MeltingApp/MeltingApp/ViewModels/CodeConfirmationViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MeltingApp.Interfaces;
+using MeltingApp.Services;
+using MeltingApp.Views.Pages;
+using Xamarin.Forms;
+
+namespace MeltingApp.ViewModels
+{
+	public class CodeConfirmationViewModel : BindableObject
+    {
+        INavigationService navigationService;
+        public CodeConfirmationViewModel ()
+		{
+		    navigationService = DependencyService.Get<INavigationService>(DependencyFetchTarget.GlobalInstance);
+		    CodeConfirmationCommand = new Command(HandleCodeConfirmationCommand);
+        }
+
+        public Command CodeConfirmationCommand { get; set; }
+
+        void HandleCodeConfirmationCommand()
+        {
+
+            //Code for the api call
+
+            navigationService.SetRootPage<MainPage>();
+        }
+    }
+}

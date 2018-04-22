5f328dc0762261e21485b180d35c75257d03583d
lfiguerola <laurafipe@gmail.com>
2018-04-22 19:14:12 +0200

    color resources added

---
+++ MeltingApp/MeltingApp/Resources/Colors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MeltingApp.Resources
+{
+    public class Colors
+    {
+        public static Color SuperiorBar = Color.FromHex("#B52E2E51");
+        public static Color LoginRegisterLettersFields = Color.FromHex("#C8B7B7");
+        public static Color LoginRegisterButtonBackground = Color.FromHex("#CCCCCC");
+        public static Color LoginButtonBorder = Color.FromHex("#5C5CA1");
+        public static Color RegisterButtonBorder = Color.FromHex("#191931");
+
+
+
+
+    }
+}

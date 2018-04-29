8e327b9a718a695735d817694b172a6cb370318a
lfiguerola <laura.figuerola@est.fib.upc.edu>
2018-04-30 00:58:42 +0200

    fields in lower case added

---
+++ MeltingApp/MeltingApp/Behaviors/FieldsLowerCaseBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace MeltingApp.Behaviors
+{
+    class FieldsLowerCaseBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += bindable_TextChanged;
+        }
+
+        void bindable_TextChanged(object sender, TextChangedEventArgs args)
+        {
+            Entry entry = (Entry)sender;
+            entry.Text = entry.Text.ToLower();
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= this.bindable_TextChanged;
+        }
+    }
+}

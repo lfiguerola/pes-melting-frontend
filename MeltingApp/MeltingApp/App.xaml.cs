f7e70b90f574771883c93407cc70d0b80baf8dfc
Adolfff <adolfoo.bcn@gmail.com>
2018-05-04 16:50:09 +0200

    Botton arreglat, no mostra la info encara de la universitat

--- MeltingApp/MeltingApp/App.xaml.cs
+++ MeltingApp/MeltingApp/App.xaml.cs
@@ -53,6 +53,7 @@ namespace MeltingApp
             navigationService.RegisterPage<CodeConfirmation>();
             navigationService.RegisterPage<RegisterPage>();
             navigationService.RegisterPage<LoginPage>();
+            navigationService.RegisterPage<StaticInfoPage>();
         }
     }
 }

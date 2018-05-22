f49cb4fdd35bc60edf82a57ee5a9cf9404766218
Pau Carbonell <uapllenobrac@gmail.com>
2018-05-22 20:50:11 +0200

    Solucionar error de merge de branques

--- MeltingApp/MeltingApp/Services/ApiClientService.cs
+++ MeltingApp/MeltingApp/Services/ApiClientService.cs
@@ -46,7 +46,6 @@ namespace MeltingApp.Services
             {new Tuple<Type, string>(typeof(Event), ApiRoutes.Methods.ShowEvent), ApiRoutes.Endpoints.ShowEvent},
             {new Tuple<Type, string>(typeof(User), ApiRoutes.Methods.GetProfileUser), "/users/11" + ApiRoutes.Endpoints.GetProfileUser },
             {new Tuple<Type, string>(typeof(StaticInfo), ApiRoutes.Methods.ShowFacultyInfo), ApiRoutes.Endpoints.ShowFacultyInfo},
-            {new Tuple<Type, string>(typeof(User), ApiRoutes.Methods.GetProfileUser), "/users/11" + ApiRoutes.Endpoints.GetProfileUser },
             {new Tuple<Type, string>(typeof(IEnumerable<Event>), ApiRoutes.Methods.GetAllEvents), ApiRoutes.Endpoints.GetAllEvents },
         };
 

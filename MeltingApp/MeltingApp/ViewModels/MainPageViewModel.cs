30d592a8f9b9a6df2b099572bb7162d80504d899
Adolfff <adolfoo.bcn@gmail.com>
2018-05-24 10:28:15 +0200

    Creacio vista Finder i millora info estatica

--- MeltingApp/MeltingApp/ViewModels/MainPageViewModel.cs
+++ MeltingApp/MeltingApp/ViewModels/MainPageViewModel.cs
@@ -33,6 +33,7 @@ namespace MeltingApp.ViewModels
         public Command NavigateToGetAllEventsCommand { get; set; }
 	    public Command InfoEventCommand { get; set; }
 	    public Command NavigateToViewEventPageCommand { get; set; }
+        public Command NavigateToFinderPage { get; set; }
 
 
 
@@ -48,8 +49,9 @@ namespace MeltingApp.ViewModels
 		    ViewProfileCommand = new Command(HandleViewProfileCommand);
 		    InfoEventCommand = new Command(HandleInfoEventCommand);
 		    UploadImageCommand = new Command(HandleUploadImageCommand);
+            NavigateToFinderPage = new Command(HandleFinderCommand);
 
-		    NavigateToViewEventPageCommand = new Command(HandleNavigateToViewEventPageCommand);
+            NavigateToViewEventPageCommand = new Command(HandleNavigateToViewEventPageCommand);
             Event = new Event();
 		    EventSelected = new Event(); 
             User = new User();
@@ -229,18 +231,15 @@ namespace MeltingApp.ViewModels
                     DependencyService.Get<IOperatingSystemMethods>().ShowToast(responseMessage);
                 }
             });
-            /* = new StaticInfo()
-             {
-                 address = "Carrer Sparragus",
-                 name = "UPC",
-                 latitude = 41.4113891882873F,
-                 longitude = 41.4113891882873F,
-                 telephone = "123456789"
-             };
-             _navigationService.PushAsync<StaticInfoPage>(this);*/
         }
 
-	    private async void HandleUploadImageCommand()
+        void HandleFinderCommand()
+        {
+            /*rellenar*/
+            _navigationService.PushAsync<FinderPage>(this);
+        }
+
+        private async void HandleUploadImageCommand()
         {
             if (!CrossMedia.Current.IsPickPhotoSupported)
             {

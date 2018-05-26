259568f841ef926bd877591c419d93d76a168dce
lfiguerola <laurafipe@gmail.com>
2018-05-26 20:32:25 +0200

    delete and deleteCollection added

--- MeltingApp/MeltingApp/ViewModels/ProfileViewModel.cs
+++ MeltingApp/MeltingApp/ViewModels/ProfileViewModel.cs
@@ -20,12 +20,11 @@ namespace MeltingApp.ViewModels
         private Faculty _faculty;
         private IEnumerable<Faculty> _faculties;
         private string _responseMessage;
-
+        private ImageSource _image1;
         private int countriesSelectedIndex;
         private string SelectedCountry;
         private University _mySelectedUniversity;
         private Faculty _mySelectedFaculty;
-
         public Command NavigateToEditProfilePageCommand { get; set; }
         public Command SaveEditProfileCommand { get; set; }
         public Command CreateProfileCommand { get; set; }

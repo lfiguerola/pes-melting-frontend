4dead0a9e06f2f0b93e92e28924bbe11e6079a1d
lfiguerola <laurafipe@gmail.com>
2018-05-26 20:32:25 +0200

    delete and deleteCollection added

--- MeltingApp/MeltingApp/Interfaces/IFileLocatorService.cs
+++ MeltingApp/MeltingApp/Interfaces/IFileLocatorService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MeltingApp.Interfaces
+﻿namespace MeltingApp.Interfaces
 {
     public interface IFileLocatorService
     {

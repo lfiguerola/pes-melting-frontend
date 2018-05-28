b8e10d63e2e167638d01775e268b8063e22d22f9
Adolfff <adolfoo.bcn@gmail.com>
2018-05-28 21:12:19 +0200

    Estructura creada, falta tratar los datos

--- MeltingApp/MeltingApp/ViewModels/FinderViewModel.cs
+++ MeltingApp/MeltingApp/ViewModels/FinderViewModel.cs
@@ -16,25 +16,57 @@ namespace MeltingApp.ViewModels
         private IApiClientService _apiClientService;
         private StaticInfo _staticInfo;
         private Event _event;
-        private string _selectedFilter;
         private string _responseMessage;
+        private string FilterToApply;
 
-        public String filter
+        private int _selectedFilter;
+
+        public Command ApplyFinderButtonCommand { get; set; }
+        public int SelectedFilterIndex
         {
             get { return _selectedFilter; }
             set
             {
-                _selectedFilter = value;
-                OnPropertyChanged(nameof(filter));
+                if (_selectedFilter != value)
+                {
+                    _selectedFilter = value;
+                    OnPropertyChanged(nameof(SelectedFilterIndex));
+                    FilterToApply = Filters[SelectedFilterIndex];
+                }
             }
         }
 
+        private List<string> filters = new List<string>
+        {
+            "Faculties",
+            "Users",
+            "Universities",
+        };
+
+        public List<string> Filters => filters;
         public FinderViewModel()
         {
+            ApplyFinderButtonCommand = new Command(HandleApplyFinder);
             _navigationService = DependencyService.Get<INavigationService>();
             _apiClientService = DependencyService.Get<IApiClientService>();
             _staticInfo = new StaticInfo();
             _event = new Event();
+        }
+
+        void HandleApplyFinder()
+        {
+            if (FilterToApply.Equals("Universities"))
+            {
+
+            }
+            else if (FilterToApply.Equals("Users"))
+            {
+
+            }
+            else if (FilterToApply.Equals("Faculties"))
+            {
+
+            }
         }
     }
 }

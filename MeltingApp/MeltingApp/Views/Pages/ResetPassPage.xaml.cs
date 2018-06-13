0a8740e13c24a029e87a6f7071717e5ce00abe2a
lfiguerola <laurafipe@gmail.com>
2018-06-14 00:01:00 +0200

    afegit changepassword button i arreglat bug del forgotpage

--- MeltingApp/MeltingApp/Views/Pages/ResetPassPage.xaml.cs
+++ MeltingApp/MeltingApp/Views/Pages/ResetPassPage.xaml.cs
@@ -15,7 +15,7 @@ namespace MeltingApp.Views.Pages
 		public ResetPassPage ()
 		{
 			InitializeComponent ();
-		    NavigationPage.SetHasNavigationBar(this, false);
+            NavigationPage.SetHasBackButton(this, true);
         }
 	}
 }

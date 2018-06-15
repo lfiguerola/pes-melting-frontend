dbb00062735d55a532ec200027bc3fa7fb5d9c47
lfiguerola <laura.figuerola@est.fib.upc.edu>
2018-06-15 04:36:42 +0200

    search finished

--- MeltingApp/MeltingApp.Android/Resources/Resource.designer.cs
+++ MeltingApp/MeltingApp.Android/Resources/Resource.designer.cs
@@ -2192,26 +2192,26 @@ namespace MeltingApp.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020121
-			public const int avd_hide_password_1 = 2130837793;
+			// aapt resource value: 0x7f020124
+			public const int avd_hide_password_1 = 2130837796;
 			
-			// aapt resource value: 0x7f020122
-			public const int avd_hide_password_2 = 2130837794;
+			// aapt resource value: 0x7f020125
+			public const int avd_hide_password_2 = 2130837797;
 			
-			// aapt resource value: 0x7f020123
-			public const int avd_hide_password_3 = 2130837795;
+			// aapt resource value: 0x7f020126
+			public const int avd_hide_password_3 = 2130837798;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020124
-			public const int avd_show_password_1 = 2130837796;
+			// aapt resource value: 0x7f020127
+			public const int avd_show_password_1 = 2130837799;
 			
-			// aapt resource value: 0x7f020125
-			public const int avd_show_password_2 = 2130837797;
+			// aapt resource value: 0x7f020128
+			public const int avd_show_password_2 = 2130837800;
 			
-			// aapt resource value: 0x7f020126
-			public const int avd_show_password_3 = 2130837798;
+			// aapt resource value: 0x7f020129
+			public const int avd_show_password_3 = 2130837801;
 			
 			// aapt resource value: 0x7f020055
 			public const int background = 2130837589;
@@ -2789,11 +2789,11 @@ namespace MeltingApp.Droid
 			// aapt resource value: 0x7f020114
 			public const int notification_icon_background = 2130837780;
 			
-			// aapt resource value: 0x7f02011f
-			public const int notification_template_icon_bg = 2130837791;
+			// aapt resource value: 0x7f020122
+			public const int notification_template_icon_bg = 2130837794;
 			
-			// aapt resource value: 0x7f020120
-			public const int notification_template_icon_low_bg = 2130837792;
+			// aapt resource value: 0x7f020123
+			public const int notification_template_icon_low_bg = 2130837795;
 			
 			// aapt resource value: 0x7f020115
 			public const int notification_tile_bg = 2130837781;
@@ -2805,25 +2805,34 @@ namespace MeltingApp.Droid
 			public const int password = 2130837783;
 			
 			// aapt resource value: 0x7f020118
-			public const int plus_128 = 2130837784;
+			public const int picker_24 = 2130837784;
 			
 			// aapt resource value: 0x7f020119
-			public const int plus_32 = 2130837785;
+			public const int plus_128 = 2130837785;
 			
 			// aapt resource value: 0x7f02011a
-			public const int plus_64 = 2130837786;
+			public const int plus_32 = 2130837786;
 			
 			// aapt resource value: 0x7f02011b
-			public const int profile = 2130837787;
+			public const int plus_64 = 2130837787;
 			
 			// aapt resource value: 0x7f02011c
-			public const int profile_pick = 2130837788;
+			public const int profile = 2130837788;
 			
 			// aapt resource value: 0x7f02011d
-			public const int search_64 = 2130837789;
+			public const int profile_pick = 2130837789;
 			
 			// aapt resource value: 0x7f02011e
-			public const int user = 2130837790;
+			public const int search_24 = 2130837790;
+			
+			// aapt resource value: 0x7f02011f
+			public const int search_32 = 2130837791;
+			
+			// aapt resource value: 0x7f020120
+			public const int search_64 = 2130837792;
+			
+			// aapt resource value: 0x7f020121
+			public const int user = 2130837793;
 			
 			static Drawable()
 			{
